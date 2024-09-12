859522fee17c22afe8721c2edd3b99247c65ad80
TsvetislavRangelov <cveti2002mnt@gmail.com>
2024-09-12 03:08:15 +0200

    Remove build output and finish implementation in Main.java

--- Binary/src/main/java/Binary.g4
+++ Binary/src/main/java/Binary.g4
@@ -4,11 +4,11 @@ grammar Binary;
 
 line:		expr+ EOF;
 
-expr:		expr binary_op expr | INTEGER;
+expr:		expr binary_op expr | INTEGER; // this rule is left recursive
 
 binary_op:	BINARY_OP;	
 
-//Lexer Rules
+// Lexer Rules
 
 BINARY_OP:	'*' | '/' | '+' | '-' | '~' | '&&' | 'v' | '<<'; // v = XOR, ~ = NOT
 INTEGER:	[0-9]+;
--- Binary/src/main/java/Main.java
+++ Binary/src/main/java/Main.java
@@ -1,8 +1,6 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
-
-
 public class Main
 {
     public static void main(String[] args) throws Exception
@@ -14,15 +12,17 @@ public class Main
 
         // Prints the lexer's vocabulary up to WHITESPACE
         Vocabulary vocab = lexer.getVocabulary();
-        for(int i = 0; i < vocab.getMaxTokenType(); i++){
+        System.out.println("Lexer vocabulary: ");
+        // we start looping from 1 since token enumeration starts from 1.
+        for(int i = 1; i < vocab.getMaxTokenType(); i++){
             System.out.println(vocab.getDisplayName(i));
         }
-        System.out.println("Tokens: " + tokens.getText());
 
         BinaryParser parser = new BinaryParser(tokens);
 
         ParseTree tree = parser.line();
 
+        // we pass the vocab to the listener.
         MainBinaryListener m = new MainBinaryListener(vocab);
         ParseTreeWalker.DEFAULT.walk(m, tree);
     }
@@ -31,7 +31,8 @@ public class Main
 final class MainBinaryListener extends BinaryBaseListener
 {
 
-    // we pass the vocabulary to the listener from the lexer in Main
+    // we pass the vocabulary to the listener from the lexer in Main.
+    // Vocabulary is an interface so it is optimal to DI.
     private final Vocabulary _vocab;
     MainBinaryListener(Vocabulary vocab){
         _vocab = vocab;
@@ -39,25 +40,69 @@ final class MainBinaryListener extends BinaryBaseListener
 
     @Override public void enterLine(BinaryParser.LineContext ctx)
     {
-        // TODO: investigate contents of 'ctx'
-        System.out.println(ctx.getText());
+        System.out.println("Input line(s): " + ctx.getText());
+        int additions = countAdditions(ctx.getText());
+        System.out.println("Addition count: " + additions);
+
+        int highestNumber = getLargestNumber(ctx.getText());
+        System.out.println("Largest integer is: " + highestNumber);
     }
 
     @Override public void exitLine(BinaryParser.LineContext ctx)
     {
-
-        // TODO: investigate contents of 'ctx'
         System.out.println("Exiting Line " + ctx.getText());
     }
 
-    // we use the vocab here to get the symbolic name for the rule number
+    /**
+     * Visits a terminal node in the parse tree.
+     * @param node The current node in the tree.
+     */
     @Override public void visitTerminal(TerminalNode node)
     {
+        // we use the vocab here to get the symbolic name for the rule number
+        String symbolicName = _vocab.getSymbolicName(node.getSymbol().getType());
+        int lineNumber = node.getSymbol().getLine();
 
-        System.out.println("Line: " + node.getSymbol().getLine()
+        System.out.println("Line: " + lineNumber
                 + ' '
-                + '|' + " Type: " + _vocab.getSymbolicName(node.getSymbol().getType()));
-        // TODO: print line+column, token's type, etc.
+                + '|' + " Type: " + symbolicName);
     }
-    // TODO: override other methods of 'MyGrammarBaseListener'
+
+    /**
+     * Gets the largest integer from a list of integers.
+     * @param expression The expression we're parsing.
+     * @return The highest integer in the list
+     */
+    private int getLargestNumber(String expression){
+        int highest = 0;
+        if(expression.isEmpty()){
+            return highest;
+        }
+        // we need only the numbers from within the expression.
+        String[] numbers = expression.split("[^0-9]+");
+        for(String number : numbers){
+            if(!number.isEmpty()){
+                int curr = Integer.parseInt(number);
+                if(curr > highest){
+                    highest = curr;
+                }
+            }
+        }
+        return highest;
+    }
+
+    /**
+     * Counts the additions in a line.
+     * @param line The line containing the expression(s).
+     * @return The number of additions
+     */
+    private int countAdditions(String line){
+        int result = 0;
+        for(char c : line.toCharArray()){
+            if(c == '+'){
+                result++;
+            }
+        }
+        return result;
+    }
 }
