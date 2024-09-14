c722ec9eb822bbcb9ace239e493028e00208e57c
AleksandraKrasteva <akaralekereks@gmail.com>
2024-09-14 20:54:27 +0200

    support basic calculations

--- Binary/src/main/java/Binary.g4
+++ Binary/src/main/java/Binary.g4
@@ -2,20 +2,24 @@ grammar Binary;
 
 // Parser Rules
 
-start:     (line+) + EOF;
+start:          (line+) + EOF;
 
-line:		expr + line_separator;
+line:		    expr + line_separator;
 
-expr:		expr binary_op expr | INTEGER; // this rule is left recursive
+expr:		    expr binary_op expr | INTEGER; // this rule is left recursive
 
-binary_op:	BINARY_OP;	
+binary_op:	    BINARY_OP;	
 
 line_separator: LINE_SEPARATOR;
 
 // Lexer Rules
 
-BINARY_OP:	'*' | '/' | '+' | '-' | '~' | '&&' | 'v' | '<<'; // v = XOR, ~ = NOT
-LINE_SEPARATOR: ';';
-INTEGER:	[0-9]+;
+BINARY_OP:	    '*' | '/' | '+' | '-' | '~' | 'v' | '<<'; // v = XOR, ~ = NOT
 
-WHITESPACE:	[ \t\r\n]+ -> skip;
+LINE_SEPARATOR: ';';
+
+INTEGER:	    [0-9]+;
+
+COMMENT:        '//' ~[\r\n]* -> skip;
+
+WHITESPACE:	    [ \t\r\n]+ -> skip;
--- Binary/src/main/java/Main.java
+++ Binary/src/main/java/Main.java
@@ -1,10 +1,17 @@
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Vocabulary;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class Main
 {
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         CharStream input = CharStreams.fromStream(System.in);
         BinaryLexer lexer = new BinaryLexer(input);
 
@@ -30,33 +37,55 @@ public class Main
     }
 }
 
-final class MainBinaryListener extends BinaryBaseListener
-{
+
+final class MainBinaryListener extends BinaryBaseListener {
 
     // we pass the vocabulary to the listener from the lexer in Main.
     // Vocabulary is an interface so it is optimal to DI.
     private final Vocabulary _vocab;
-    MainBinaryListener(Vocabulary vocab){
+    MainBinaryListener(Vocabulary vocab) {
         _vocab = vocab;
     }
 
-    @Override public void enterLine(BinaryParser.LineContext ctx)
-    {
+    private List<Integer> nums=new ArrayList<Integer>();
+    private List<String> binary_ops=new ArrayList<String>();
+
+    @Override public void enterLine(BinaryParser.LineContext ctx) {
+        nums.clear();
+        binary_ops.clear();
         displayInfo(ctx.getText());
     }
 
-    @Override public void exitLine(BinaryParser.LineContext ctx)
-    {
-        System.out.println("\033[1mExiting Line\033[0m " + ctx.getText());
+    @Override public void exitLine(BinaryParser.LineContext ctx) {
         System.out.println();
+        if(isCalculationSupported()){
+            int result = processCalculation();
+            System.out.println("\033[1mResult is:\033[0m " + result );
+        }
+        else{
+            System.out.println("\033[1mResult:\033[0m expression is not supported");
+
+        }
+        System.out.println("\033[1mExiting Line\033[0m " + ctx.getText());
+        System.out.println("---------------------------");
+    }
+
+    @Override public void exitExpr(BinaryParser.ExprContext ctx) {
+        // has reached last leaf
+        if(ctx.getChildCount() == 1){
+            nums.add(Integer.parseInt(ctx.getText()));
+        }
+    }
+
+    @Override public void exitBinary_op(BinaryParser.Binary_opContext ctx) {
+        binary_ops.add(ctx.getText());
     }
 
     /**
      * Visits a terminal node in the parse tree.
      * @param node The current node in the tree.
      */
-    @Override public void visitTerminal(TerminalNode node)
-    {
+    @Override public void visitTerminal(TerminalNode node) {
         // we use the vocab here to get the symbolic name for the rule number
         String symbolicName = _vocab.getSymbolicName(node.getSymbol().getType());
         int lineNumber = node.getSymbol().getLine();
@@ -71,7 +100,7 @@ final class MainBinaryListener extends BinaryBaseListener
      * as well as the highest integer in the parse line.
      * @param parseLine the line the parser is reading.
      */
-    private void displayInfo(String parseLine){
+    private void displayInfo(String parseLine) {
         System.out.println("\033[1mInput line(s):\033[0m " + parseLine);
         int additions = countAdditions(parseLine);
         System.out.println("\033[1mAddition count:\033[0m " + additions);
@@ -86,7 +115,7 @@ final class MainBinaryListener extends BinaryBaseListener
      * @param expression The expression we're parsing.
      * @return The highest integer in the list
      */
-    private int getLargestNumber(String expression){
+    private int getLargestNumber(String expression) {
         int highest = 0;
         if(expression.isEmpty()){
             return highest;
@@ -109,7 +138,7 @@ final class MainBinaryListener extends BinaryBaseListener
      * @param line The line containing the expression.
      * @return The number of additions
      */
-    private int countAdditions(String line){
+    private int countAdditions(String line) {
         int result = 0;
         for(char c : line.toCharArray()){
             if(c == '+'){
@@ -119,4 +148,61 @@ final class MainBinaryListener extends BinaryBaseListener
         return result;
     }
 
+    /**
+     * Goes through the collected numbers and operations and calculates them.
+     * @return the result from the expression.
+     */
+    private int processCalculation() {
+        int result = 0; 
+        for (int i = 0; i< nums.size(); i++) {
+            if (i == 0){
+                result = nums.get(i); 
+            }
+            else{
+                result = calculate(result, nums.get(i), binary_ops.get(i-1));
+            }
+        }
+        return result; 
+    }
+
+    /**
+     * Checks if the current expression can be calculated.
+     * @return true if the expression is supported.
+     */
+    private boolean isCalculationSupported() {
+        boolean hasDivOrMul = binary_ops.contains("/") || binary_ops.contains("*");
+        boolean hasAddOrSub = binary_ops.contains("+") || binary_ops.contains("-");
+        boolean hasNegation = binary_ops.contains("~");
+
+        if(hasNegation || (hasAddOrSub && hasDivOrMul)){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Calculates the expression based on the operation.
+     * @param a first digit.
+     * @param b second digit.
+     * @param op operation.
+     * @return the calculated result.
+     */
+    private int calculate(int a, int b, String op) {
+        switch(op) {
+            case "+":
+              return a + b; 
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            case "<<":
+                return a << b;
+            case "v": 
+                return a ^ b;
+            default:
+                return 0;                          
+          }       
+    }
 }
